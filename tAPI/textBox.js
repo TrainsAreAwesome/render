5a0ec412946b5ef4ecdfc961e2def1f565f18314
Morgan <morganlote@gmail.com>
2023-11-08 22:23:01 +0000

    dont use this version3

--- tAPI/textBox.js
+++ tAPI/textBox.js
@@ -1,25 +1,52 @@
-export {textBox}
+import { render } from "./render.js"
+export { textBox }
 import { Chalk } from "chalk"
-import { font } from "./font"
+//import { font } from "./font.js"
 
-let textBox = (specifiedText, specifiedTextColour, specifiedBoxColour) => {
+let textBox = (text, textColour, boxColour) => {
 
-    let textColour = "chalk.bgWhite.hidden"
-    let boxColour = "chalk.bgWhite.hidden"
+    let boxOutLinep = boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour + boxColour
+    let boxOutLine = boxOutLinep + boxOutLinep + boxOutLinep + boxOutLinep + boxOutLinep
+
+    let a = 
+    ["##",
+    "#  #",
+   "#    #",
+  "########",
+ "#        #",
+"#          #"  ]
+
+
     let commandConstuct = ""
+    let i = 0
+    while (i < 10) {
+        commandConstuct = ""
+        if (i === 0 || i === 9) {
+            commandConstuct = boxOutLine
+        } else {
+            if (i === 1 || i === 8) {
+                commandConstuct = ""
+          } else {
+                commandConstuct = "r"
+                let o = 0
+                while (o < a[i].length) {
+                commandConstuct = commandConstuct + textColour
+                    ++o
+               }
 
-    if (specifiedTextColour =~ null) {
-        textColour = specifiedTextColour
-    }
 
-    if (specifiedBoxColour =~ null) {
-        boxColour = specifiedBoxColour
-    }
+            }
+        }
 
-    
-    while (i = 0, i < font.a.length) {
-        commandConstuct = font.a[i].length
+
+
+
+
+
+        render(commandConstuct)
+        //console.log(commandConstuct)
+        ++i
     }
 }
 
-textBox()
+textBox("a", "e", "e")

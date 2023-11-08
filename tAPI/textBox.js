7132ecb4ca67e83c78a5d53c1d0cc413535c5131
Morgan <morganlote@gmail.com>
2023-11-08 21:35:55 +0000

    dont use this version2

--- tAPI/textBox.js
+++ tAPI/textBox.js
@@ -1,11 +1,12 @@
 export {textBox}
 import { Chalk } from "chalk"
-
+import { font } from "./font"
 
 let textBox = (specifiedText, specifiedTextColour, specifiedBoxColour) => {
 
     let textColour = "chalk.bgWhite.hidden"
     let boxColour = "chalk.bgWhite.hidden"
+    let commandConstuct = ""
 
     if (specifiedTextColour =~ null) {
         textColour = specifiedTextColour
@@ -16,5 +17,9 @@ let textBox = (specifiedText, specifiedTextColour, specifiedBoxColour) => {
     }
 
     
-
+    while (i = 0, i < font.a.length) {
+        commandConstuct = font.a[i].length
+    }
 }
+
+textBox()
